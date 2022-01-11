da004ee4e48d36726b41185c91acd72734372c6d
sun00120 <sun00120@algonquinlive.com>
2022-01-10 22:15:26 -0500

    Read JSON file into variable, print arrays by using forEach, filter, and charAt method

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,21 @@
-'use strict'
+"use strict";
 
 // 1. Read the JSON file into a variable called students
+const students = require("./students.json");
 
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
+students.forEach(function (students) {
+  console.log(`Hello ${students.firstName} ${students.lastName}`);
+});
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const filterLastName = students.filter(
+  (student) => student.lastName.charAt(0) === "D"
+);
+
+console.log(
+  "\n" + `Count of last names starting with D is ${filterLastName.length}`
+);
